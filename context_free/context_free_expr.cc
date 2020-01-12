f724676be5d1e6bf8a483e7305b30a8b3a0a1c98
syple000 <1519761174@qq.com>
2020-01-12 22:01:12 +0800

    update

--- context_free/context_free_expr.cc
+++ context_free/context_free_expr.cc
@@ -1,60 +1,35 @@
 
 #include "./context_free_expr.h"
 
-int ContextFreeExprUtil::IsNullable(ContextFreeSymbol* symbol, std::set<ContextFreeSymbol*>& dependentSymbols) {
-    if (symbol->_nullable != 2) {
-        return symbol->_nullable;
+void ContextFreeExprUtil::GenNullable() {
+    std::vector<ContextFreeSymbol*> symbolVec(this->_symbolMap.size(), nullptr);
+    for (auto& it : this->_symbolMap) {
+        symbolVec[it.second->_number] = it.second;
     }
 
-    const auto& exprs = this->_exprMap.find(symbol->_key)->second;
-    dependentSymbols.insert(symbol);
-
-    int isNullable = 0;
-    for (auto expr : exprs) {
-        int nullable = ContextFreeExprUtil::IsExprNullable(expr, dependentSymbols);
-        if (nullable == 1) {
-            symbol->_nullable = nullable;
-            isNullable = 1;
-            break;
-        }
-        if (nullable == 2) {
-            isNullable = 2;
-        }
-    }
-
-    if (isNullable == 2) {
-        // 存在循环判定，is nullable被赋值0
-        isNullable = 0;
-    }
-
-    dependentSymbols.erase(symbol);
-    symbol->_nullable = isNullable;
-    return isNullable;
-}
-
-int ContextFreeExprUtil::IsExprNullable(ContextFreeExpr* expr, std::set<ContextFreeSymbol*>& dependentSymbols) {
-    if (expr->_nullable != 2) {
-        return expr->_nullable;
-    }
-    int nullable = 1;
-    for (int i = 0; i < expr->_production.size(); i++) {
-        int nodeNullable = expr->_production[i]->_nullable;
-        if (nodeNullable == 2) {
-            if (dependentSymbols.find(expr->_production[i]) == dependentSymbols.end()) {
-                nodeNullable = ContextFreeExprUtil::IsNullable(expr->_production[i], dependentSymbols);
+    std::map<int, std::pair<std::vector<std::set<int>>, std::vector<std::set<int>>>> dependentInfo;
+    for (auto symbol : symbolVec) {
+        if (symbol->_state._nullable == 2) {
+            int nullable = 0;
+            std::vector<std::set<int>> dependingVec, dependedVec;
+            auto& exprs = this->_exprMap.find(symbol->_key)->second;
+            for (auto& expr : exprs) {
+                std::set<int> dependingSet, dependedSet;
+                int exprNullable = 1;
+                for (auto& innerSymbol : expr->_production) {
+                    if (innerSymbol->_state._nullable == 0) {
+                        exprNullable = 0;
+                        break;
+                    } else {
+                        if (innerSymbol->_state._nullable == 2) {
+                            exprNullable = 2;
+                        }
+                        continue;
+                    }
+                }
+                if ()
             }
         }
-
-        if (nodeNullable == 1) {
-            continue;
-        } else if (nodeNullable == 2) {
-            nullable = 2;
-            continue;
-        } else {
-            nullable = 0;
-            break;
-        }
     }
-    expr->_nullable = nullable;
-    return nullable;
+
 }
--- context_free/context_free_expr.h
+++ context_free/context_free_expr.h
@@ -8,43 +8,31 @@
 #define CONTEXT_FREE 1
 
 struct ContextFreeExpr;
+struct ContextFreeSymbol;
+
+struct SymbolState {
+    // 不可空：0， 可空：1， 未知：2
+    int _nullable;
+    bool _isTerminator;
+    std::set<ContextFreeSymbol*> _first, _last;
+    std::map<ContextFreeExpr*, int> _positionInExpr;
+
+    SymbolState(int nullable, int isTerminator) : _nullable(nullable), _isTerminator(isTerminator) {}
+};
 
 struct ContextFreeSymbol {
     std::string _key;
     std::string _keyRegExpr;
-    bool _isTerminator;
-    // 不可空：0， 可空：1， 未知：2
-    int _nullable;
     int _number;
+    SymbolState _state;
 
-    std::set<ContextFreeSymbol*> *_first, *_last;
-    std::map<ContextFreeExpr*, int>* _positionInExpr;
-
-    ContextFreeSymbol(const std::string& key, const std::string& keyRegExpr, bool isTerminator, int nullable, int number) 
-        : _key(key), _keyRegExpr(keyRegExpr), _isTerminator(isTerminator), _nullable(nullable), _number(number) {
-        this->_first = new std::set<ContextFreeSymbol*>();
-        if (this->_isTerminator) {
-            this->_last = nullptr;
-            this->_positionInExpr = nullptr;
-            this->_first->insert(this);
-        } else {
-            this->_last = new std::set<ContextFreeSymbol*>();
-            this->_positionInExpr = new std::map<ContextFreeExpr*, int>();
-        }
-    }
-
-    ~ContextFreeSymbol() {
-        delete this->_first;
-        delete this->_last;
-        delete this->_positionInExpr;
-    }
+    ContextFreeSymbol(const std::string& key, const std::string& keyRegExpr,  int number, bool isTerminator, int nullable) 
+        : _key(key), _keyRegExpr(keyRegExpr), _number(number), _state(nullable, isTerminator) {} 
 };
 
 struct ContextFreeExpr {
     ContextFreeSymbol* _sourceSymbol;
     std::vector<ContextFreeSymbol*> _production;
-    // 不可空：0， 可空：1， 未知：2
-    int _nullable;
 };
 
 struct ContextFreeExprState {
@@ -58,8 +46,7 @@ public:
     std::unordered_map<std::string, ContextFreeSymbol*> _symbolMap;
     std::unordered_map<std::string, std::set<ContextFreeExpr*>> _exprMap;
 
-    int IsNullable(ContextFreeSymbol* symbol, std::set<ContextFreeSymbol*>& dependentSymbols);
-    int IsExprNullable(ContextFreeExpr* expr, std::set<ContextFreeSymbol*>& dependentSymbols);
+    void GenNullable();
     void GenFirstOfExprNode(const std::string& key);
     void GenLastOfExprNode(const std::string& key);
     void GetFirstOfProductionBody(const std::vector<ContextFreeSymbol*>& symbolVec, int index);
