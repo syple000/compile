9415c1fb91ff46ba76d621ef05e5891faa912ca7
syple000 <1519761174@qq.com>
2020-01-13 23:00:37 +0800

    update

--- context_free/context_free_expr.h
+++ context_free/context_free_expr.h
@@ -2,37 +2,75 @@
 #include <vector>
 #include <set>
 #include <map>
+#include <list>
 #include <unordered_map>
+
+#include "../observer_pattern/observer_pattern.h"
 
 #ifndef CONTEXT_FREE
 #define CONTEXT_FREE 1
 
 struct ContextFreeExpr;
 struct ContextFreeSymbol;
+struct ContextFreeExprState;
+class ContextFreeExprUtil;
 
-struct SymbolState {
-    // 不可空：0， 可空：1， 未知：2
-    int _nullable;
-    bool _isTerminator;
+struct SymbolCalcCache {
+    static bool NullableInfoObserverUpdate(const ContextFreeSymbol* subject, ContextFreeSymbol* observer) {
+        
+    }
+
     std::set<ContextFreeSymbol*> _first, _last;
     std::map<ContextFreeExpr*, int> _positionInExpr;
+    Subject<ContextFreeSymbol*, ContextFreeSymbol*> _nullableInfoSubject;
 
-    SymbolState(int nullable, int isTerminator) : _nullable(nullable), _isTerminator(isTerminator) {}
+    SymbolCalcCache(ContextFreeSymbol* symbol) : _nullableInfoSubject(symbol) {}
 };
 
 struct ContextFreeSymbol {
     std::string _key;
     std::string _keyRegExpr;
     int _number;
-    SymbolState _state;
+    // 不可空：0， 可空：1， 未知：2
+    int _nullable;
+    bool _isTerminator;
+    SymbolCalcCache *calcCache = nullptr;
 
     ContextFreeSymbol(const std::string& key, const std::string& keyRegExpr,  int number, bool isTerminator, int nullable) 
-        : _key(key), _keyRegExpr(keyRegExpr), _number(number), _state(nullable, isTerminator) {} 
+        : _key(key), _keyRegExpr(keyRegExpr), _number(number), _nullable(nullable), _isTerminator(isTerminator) {
+        calcCache = new SymbolCalcCache(this);
+    } 
+
+    static int IsExprNullable(ContextFreeExpr* expr, std::set<ContextFreeSymbol*>& dependings) {
+        if (expr->_nullable != 2) {
+            return expr->_nullable;
+        }
+        int nullable = 1;
+        for (auto innerSymbol : expr->_production) {
+            if (innerSymbol->_nullable == 0) {
+                nullable = 0;
+                break;
+            } else if (innerSymbol->_nullable == 2){
+                dependings.insert(innerSymbol);
+                nullable = 2;
+            }
+        }
+        return nullable;
+    }
+
+    static int IsSymbolNullable(ContextFreeSymbol* symbol, const std::set<ContextFreeExpr*>& exprs) {
+        if (symbol->_nullable != 2) {
+            
+        }
+        std::set<ContextFreeSymbol*> dependings;
+
+    }
 };
 
 struct ContextFreeExpr {
     ContextFreeSymbol* _sourceSymbol;
     std::vector<ContextFreeSymbol*> _production;
+    int _nullable = 2;
 };
 
 struct ContextFreeExprState {
@@ -41,15 +79,15 @@ struct ContextFreeExprState {
 };
 
 class ContextFreeExprUtil {
-public:
-//private:
+private:
     std::unordered_map<std::string, ContextFreeSymbol*> _symbolMap;
     std::unordered_map<std::string, std::set<ContextFreeExpr*>> _exprMap;
 
     void GenNullable();
-    void GenFirstOfExprNode(const std::string& key);
-    void GenLastOfExprNode(const std::string& key);
-    void GetFirstOfProductionBody(const std::vector<ContextFreeSymbol*>& symbolVec, int index);
+
+    void GenFirst();
+
+    void GenLast();
 };
 
 #endif
---
+++ observer_pattern/observer_pattern.h
@@ -0,0 +1,88 @@
+#include <set>
+
+#ifndef OBSERVER_PATTERN
+#define OBSERVER_PATTERN 1
+
+template<typename T, typename K>
+class Subject;
+
+template<typename T, typename K>
+class Observer;
+
+template<typename T, typename K>
+class Subject {
+private:
+    T _subject;
+    std::set<Observer<T, K>> _observers;
+    bool _updated;
+
+public:
+    Subject(const T&);
+    void SetUpdated(bool);
+    void NotifyObservers();
+    void Insert(Observer<T, K>&&);
+    void Remove(Observer<T, K>&);
+};
+
+template<typename T, typename K>
+Subject<T, K>::Subject(const T& subject) {
+    this->_subject = subject;
+    this->_updated = false;
+}
+
+template<typename T, typename K>
+void Subject<T, K>::SetUpdated(bool updated) {
+    this->_updated = updated;
+}
+
+template<typename T, typename K>
+void Subject<T, K>::NotifyObservers() {
+    if (this->_updated) {
+        for (auto itr = this->_observers.begin(); itr != this->_observers.end(); ) {
+            if (itr->Update(this->_subject)) {
+                this->_observers.erase(itr++);
+            }
+        }
+    }
+}
+
+template<typename T, typename K>
+void Subject<T, K>::Insert(Observer<T, K>&& observer) {
+    this->_observers.insert(std::move(observer));
+}
+
+template<typename T, typename K>
+void Subject<T, K>::Remove(Observer<T, K>& observer) {
+    this->_observers.erase(observer);
+}
+
+template<typename T, typename K>
+class Observer {
+private:
+    K _observer;
+    bool (*_update)(const T&, const K&);
+
+public:
+    Observer(const K&, bool(*update)(const T&, const K&));
+    // return value: wether to remove itsalf from the observer list
+    bool Update(const T&);
+    bool operator< (const Observer& observer) const;
+};
+
+template<typename T, typename K>
+Observer<T, K>::Observer(const K& observer, bool(*update)(const T&, const K&)) {
+    this->_observer = observer;        
+    this->_update = update;
+}
+
+template<typename T, typename K>
+bool Observer<T, K>::Update(const T& subject) {
+    return this->_update(subject, this->_observer);
+}
+
+template<typename T, typename K>
+bool Observer<T, K>::operator< (const Observer& observer) const {
+    return this->_observer < observer._observer;
+}
+
+#endif
