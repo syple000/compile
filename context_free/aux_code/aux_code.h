1e78dd2b71982279408200e4599c08b7dca8c5df
syple000 <1519761174@qq.com>
2020-02-17 02:46:25 -0500

    update

--- context_free/aux_code/aux_code.h
+++ context_free/aux_code/aux_code.h
@@ -14,14 +14,14 @@ public:
     std::unordered_map<std::string, void(*)(CfTreeNode*, std::vector<CfTreeNode*>&)> funcRegistry;
     static void _func_1_(CfTreeNode* pnode, std::vector<CfTreeNode*>& knodes) {
 
-        CfTreeNode::MoveAttributes(knodes[knodes.size() - 2], knodes[knodes.size() - 1]);
+        CfTreeNode::MoveAttributes(((knodes.size() >= 2) ? knodes[knodes.size() - 2] : nullptr), ((knodes.size() >= 1) ? knodes[knodes.size() - 1] : nullptr));
     }
     static void _func_reduction_3_(CfTreeNode* pnode, std::vector<CfTreeNode*>& knodes) {
  
-        CfTreeNode::MoveAttributes(knodes[knodes.size() - 1], knodes[knodes.size() - 2]);
-        auto typeAttr = (TypeAttribute*)knodes[knodes.size() - 2]->GetAttribute("type");
-        auto ptrAttr = (PtrAttribute*)knodes[knodes.size() - 2]->GetAttribute("pointer");
-        auto arrAttr = (ArrAttribute*)knodes[knodes.size() - 2]->GetAttribute("array");
+        CfTreeNode::MoveAttributes(((knodes.size() >= 1) ? knodes[knodes.size() - 1] : nullptr), ((knodes.size() >= 2) ? knodes[knodes.size() - 2] : nullptr));
+        auto typeAttr = (TypeAttribute*)((knodes.size() >= 2) ? knodes[knodes.size() - 2] : nullptr)->GetAttribute("type");
+        auto ptrAttr = (PtrAttribute*)((knodes.size() >= 2) ? knodes[knodes.size() - 2] : nullptr)->GetAttribute("pointer");
+        auto arrAttr = (ArrAttribute*)((knodes.size() >= 2) ? knodes[knodes.size() - 2] : nullptr)->GetAttribute("array");
         VarType* varType = new VarType(typeAttr->_typeName, typeAttr->_typeSize, 
         ptrAttr == nullptr ? nullptr : ptrAttr->_ptr, arrAttr == nullptr ? nullptr : arrAttr->_array);
         if (ptrAttr != nullptr) {
@@ -30,8 +30,8 @@ public:
         if (arrAttr != nullptr) {
             arrAttr->_array = nullptr;
         }
-        tableManager.AddVar(varType, knodes[knodes.size() - 2]->_value, 
-            ((QualifierAttribute*)knodes[knodes.size() - 2]->GetAttribute("qualifier"))->_isStatic);
+        tableManager.AddVar(varType, ((knodes.size() >= 2) ? knodes[knodes.size() - 2] : nullptr)->_value, 
+            ((QualifierAttribute*)((knodes.size() >= 2) ? knodes[knodes.size() - 2] : nullptr)->GetAttribute("qualifier"))->_isStatic);
     }
     static void _func_reduction_4_(CfTreeNode* pnode, std::vector<CfTreeNode*>& knodes) {
 
@@ -43,62 +43,62 @@ public:
     }
     static void _func_reduction_6_(CfTreeNode* pnode, std::vector<CfTreeNode*>& knodes) {
 
-        CfTreeNode::MoveAttributes(knodes[knodes.size() - 2], pnode);
-        auto type = tableManager.GetTable(knodes[knodes.size() - 1]->_value);
+        CfTreeNode::MoveAttributes(((knodes.size() >= 2) ? knodes[knodes.size() - 2] : nullptr), pnode);
+        auto type = tableManager.GetTable(((knodes.size() >= 1) ? knodes[knodes.size() - 1] : nullptr)->_value);
         if (type == nullptr || type->_type != 1) {
-            std::cout << "type: " << knodes[knodes.size() - 1]->_value << "not found!" << std::endl;
+            std::cout << "type: " << ((knodes.size() >= 1) ? knodes[knodes.size() - 1] : nullptr)->_value << "not found!" << std::endl;
         }
-        pnode->AddAttribute(new TypeAttribute("type", knodes[knodes.size() - 1]->_value, 4));
+        pnode->AddAttribute(new TypeAttribute("type", ((knodes.size() >= 1) ? knodes[knodes.size() - 1] : nullptr)->_value, 4));
     }
     static void _func_reduction_7_(CfTreeNode* pnode, std::vector<CfTreeNode*>& knodes) {
 
-        CfTreeNode::MoveAttributes(knodes[knodes.size() - 2], pnode);
-        auto type = tableManager.GetTable(knodes[knodes.size() - 1]->_value);
+        CfTreeNode::MoveAttributes(((knodes.size() >= 2) ? knodes[knodes.size() - 2] : nullptr), pnode);
+        auto type = tableManager.GetTable(((knodes.size() >= 1) ? knodes[knodes.size() - 1] : nullptr)->_value);
         if (type == nullptr || type->_type != 1) {
-            std::cout << "type: " << knodes[knodes.size() - 1]->_value << "not found!" << std::endl;
+            std::cout << "type: " << ((knodes.size() >= 1) ? knodes[knodes.size() - 1] : nullptr)->_value << "not found!" << std::endl;
         }
-        pnode->AddAttribute(new TypeAttribute("type", knodes[knodes.size() - 1]->_value, 4));
+        pnode->AddAttribute(new TypeAttribute("type", ((knodes.size() >= 1) ? knodes[knodes.size() - 1] : nullptr)->_value, 4));
     }
     static void _func_2_(CfTreeNode* pnode, std::vector<CfTreeNode*>& knodes) {
 
-        CfTreeNode::MoveAttributes(knodes[knodes.size() - 4], knodes[knodes.size() - 1]);
-        auto arrAttr = (ArrAttribute*)knodes[knodes.size() - 1]->GetAttribute("array");
+        CfTreeNode::MoveAttributes(((knodes.size() >= 4) ? knodes[knodes.size() - 4] : nullptr), ((knodes.size() >= 1) ? knodes[knodes.size() - 1] : nullptr));
+        auto arrAttr = (ArrAttribute*)((knodes.size() >= 1) ? knodes[knodes.size() - 1] : nullptr)->GetAttribute("array");
         if (arrAttr == nullptr) {
             arrAttr = new ArrAttribute("array");
-            knodes[knodes.size() - 1]->AddAttribute(arrAttr);
+            ((knodes.size() >= 1) ? knodes[knodes.size() - 1] : nullptr)->AddAttribute(arrAttr);
         }
-        auto typeAttr = (TypeAttribute*)knodes[knodes.size() - 1]->GetAttribute("type");
+        auto typeAttr = (TypeAttribute*)((knodes.size() >= 1) ? knodes[knodes.size() - 1] : nullptr)->GetAttribute("type");
         int typeSize = typeAttr->_typeSize;
-        if (knodes[knodes.size() - 1]->GetAttribute("pointer") != nullptr) {
+        if (((knodes.size() >= 1) ? knodes[knodes.size() - 1] : nullptr)->GetAttribute("pointer") != nullptr) {
             typeSize = 4;
         }
-        arrAttr->AddNestArr(typeSize, std::stoi(knodes[knodes.size() - 2]->_value));
+        arrAttr->AddNestArr(typeSize, std::stoi(((knodes.size() >= 2) ? knodes[knodes.size() - 2] : nullptr)->_value));
     }
     static void _func_reduction_8_(CfTreeNode* pnode, std::vector<CfTreeNode*>& knodes) {
 
-        CfTreeNode::MoveAttributes(knodes[knodes.size() - 1], pnode);
+        CfTreeNode::MoveAttributes(((knodes.size() >= 1) ? knodes[knodes.size() - 1] : nullptr), pnode);
     }
     static void _func_reduction_9_(CfTreeNode* pnode, std::vector<CfTreeNode*>& knodes) {
 
-        CfTreeNode::MoveAttributes(knodes[knodes.size() - 2], pnode);
+        CfTreeNode::MoveAttributes(((knodes.size() >= 2) ? knodes[knodes.size() - 2] : nullptr), pnode);
     }
     static void _func_3_(CfTreeNode* pnode, std::vector<CfTreeNode*>& knodes) {
 
-        CfTreeNode::MoveAttributes(knodes[knodes.size() - 2], knodes[knodes.size() -1]);
-        auto ptrAttr = (PtrAttribute*)knodes[knodes.size() - 2]->GetAttribute("pointer");
+        CfTreeNode::MoveAttributes(((knodes.size() >= 2) ? knodes[knodes.size() - 2] : nullptr), ((knodes.size() >= 1) ? knodes[knodes.size() - 1] : nullptr));
+        auto ptrAttr = (PtrAttribute*)((knodes.size() >= 2) ? knodes[knodes.size() - 2] : nullptr)->GetAttribute("pointer");
         if (ptrAttr == nullptr) {
             ptrAttr = new PtrAttribute("pointer");
-            knodes[knodes.size() - 1]->AddAttribute(ptrAttr);
+            ((knodes.size() >= 1) ? knodes[knodes.size() - 1] : nullptr)->AddAttribute(ptrAttr);
         }
         ptrAttr->AddNestPtr();
     }
     static void _func_reduction_10_(CfTreeNode* pnode, std::vector<CfTreeNode*>& knodes) {
 
-        CfTreeNode::MoveAttributes(knodes[knodes.size() - 1], pnode);
+        CfTreeNode::MoveAttributes(((knodes.size() >= 1) ? knodes[knodes.size() - 1] : nullptr), pnode);
     }
     static void _func_reduction_11_(CfTreeNode* pnode, std::vector<CfTreeNode*>& knodes) {
 
-        CfTreeNode::MoveAttributes(knodes[knodes.size() - 2], pnode);
+        CfTreeNode::MoveAttributes(((knodes.size() >= 2) ? knodes[knodes.size() - 2] : nullptr), pnode);
     }
 
     void registFuncs() {
